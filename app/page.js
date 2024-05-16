17be9c9f5b8e631b15b2e2ee3ca4fc2da7f2dbb4
utkarshkushi <utkarshsuneela@gmail.com>
2024-05-16 19:28:36 +0530

    refactor: test commit

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
       <NavBar />
       <Main />
       <div>
-        hello
+        hell
       </div>
       {/* {hello()} */}
       {/* {arr = createAccount()} */}
